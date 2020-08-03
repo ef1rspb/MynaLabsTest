83ec97ff27d8c6be9ad71203c247241db5e5c7d6
Aleksandr Malina <a.malina@fbs.com>
2020-08-03 18:16:00 +0300

    Load rootVC

--- MynaLabsTest/AppDelegate.swift
+++ MynaLabsTest/AppDelegate.swift
@@ -3,11 +3,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  var window: UIWindow?
+
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Override point for customization after application launch.
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.backgroundColor = .black
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
     return true
   }
 }
--- MynaLabsTest/ViewController.swift
+++ MynaLabsTest/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  MynaLabsTest
-//
-//  Created by Aleksandr Malina on 03.08.2020.
-//  Copyright © 2020 Aleksandr Malina. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    print("hello")
   }
-
-
 }
-
