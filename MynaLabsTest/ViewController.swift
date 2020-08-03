a3ffc911c71446d7285b55142b680952a980087b
Aleksandr Malina <a.malina@fbs.com>
2020-08-03 17:48:22 +0300

    Init commit

---
+++ MynaLabsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MynaLabsTest
+//
+//  Created by Aleksandr Malina on 03.08.2020.
+//  Copyright © 2020 Aleksandr Malina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
